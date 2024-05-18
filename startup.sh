5acc4912f9ca70c4a2ea0431e931dfb4231e9f90
Ugo Guazelli <guazelli.ugo@gmail.com>
2024-05-18 13:34:38 -0300

    updated the startup

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 
-
 # Install caddy
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
@@ -15,6 +14,26 @@ sudo systemctl enable caddy
 # Verify Installation:
 sudo systemctl status caddy
 
+
+# Caddy file example  filename: Caddyfile
+
+# opus-web.com, www.opus-web.com {
+#         reverse_proxy localhost:8002
+# }
+
+# # Redirect www.paperless.opus-web.com to paperless.opus-web.com
+# www.paperless.opus-web.com {
+#         redir https://paperless.opus-web.com{uri} 301
+# }
+
+# paperless.opus-web.com {
+#         reverse_proxy localhost:8001
+# }
+
+
+# ##########################
+
+
 # DOCKER 
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
@@ -27,16 +46,16 @@ sudo apt-get install docker-compose-plugin -y
 sudo apt update -y
 sudo apt-get install git -y
 
+# Opus
+git clone https://github.com/uguazelli/opus.git
+
 
 # Wordpress
-docker compose -f wp-docker-compose.yml up -d
+cd opus/wp
+docker compose up -d
 
 # Paperless
-git clone https://github.com/uguazelli/opus.git
-cd opus
-
+cd /opus/paperless
 docker compose run --rm webserver createsuperuser
-
-
 
 echo "instalation finished, please run 'docker compose up -d' to start"
