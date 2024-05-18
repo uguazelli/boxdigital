5ca386d90a8c350b90e380ddd85e933ec5e5408f
Ugo Guazelli <guazelli.ugo@gmail.com>
2024-05-18 08:05:07 -0300

    updated wp

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Add the Caddy repository:
 
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
 
 # Install caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
 sudo apt install caddy
 
 # Caddy will automatically start after installation. You can enable it to start on boot with:
