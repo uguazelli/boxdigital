1e779b4dd6da98f210504935f86fde82fe24c96c
Ugo Guazelli <guazelli.ugo@gmail.com>
2024-05-18 06:24:03 -0300

    separating projects

--- startup.sh
+++ startup.sh
@@ -1,39 +1,19 @@
 #!/usr/bin/env bash
 
-# NGINX
-sudo apt update -y
-sudo apt install ufw -y
-sudo apt install nginx -y
+# Add the Caddy repository:
 
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow ssh
-sudo ufw allow 22
-sudo ufw enable
-sudo ufw allow http
-sudo ufw allow https
-sudo ufw status verbose
-sudo systemctl restart nginx
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
 
-#sudo nano /etc/default/ufw
-#Locate IPV6 in the file and ensure the value is yes:
-#IPV6=yes
-#Checking your Web Server
-#systemctl status nginx
-#ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
-#You will get back a few lines. You can try each in your web browser to see if they work.
-#Restart Nginx: After making  changes, restart Nginx for the changes to take effect.
+# Install caddy
+sudo apt install caddy
 
-# GIT
-sudo apt update -y
-sudo apt-get install git -y
+# Caddy will automatically start after installation. You can enable it to start on boot with:
+sudo systemctl enable caddy
 
-# Paperless
-git clone https://github.com/uguazelli/opus.git
-cd opus
-
-# Wordpress
-docker compose -f wp-docker-compose.yml up -d
+# Verify Installation:
+sudo systemctl status caddy
 
 # DOCKER 
 curl -fsSL get.docker.com -o get-docker.sh
@@ -41,7 +21,22 @@ sudo sh get-docker.sh
 
 # DOCKER COMPOSE
 sudo apt-get install docker-compose-plugin -y
+
+
+# GIT
+sudo apt update -y
+sudo apt-get install git -y
+
+
+# Wordpress
+docker compose -f wp-docker-compose.yml up -d
+
+# Paperless
+git clone https://github.com/uguazelli/opus.git
+cd opus
+
 docker compose run --rm webserver createsuperuser
+
 
 
 echo "instalation finished, please run 'docker compose up -d' to start"
